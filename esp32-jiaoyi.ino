4b75d1d8ebf37273bcec81181c98be2f679e5313
老宋 <14118593@qq.com>
2026-07-28 23:11:59 +0800

    fix: include esp_flash.h

--- esp32-jiaoyi.ino
+++ esp32-jiaoyi.ino
@@ -5,6 +5,7 @@
 #include "Arduino_GFX_Library.h"
 #include "pin_config.h"
 #include <Wire.h>
+#include "esp_flash.h"
 
 Arduino_DataBus *bus = new Arduino_ESP32QSPI(
   LCD_CS, LCD_SCLK, LCD_SDIO0, LCD_SDIO1, LCD_SDIO2, LCD_SDIO3);
